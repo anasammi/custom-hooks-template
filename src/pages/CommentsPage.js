4297267d738f6ed877df50ec8905904ffa523de5
anasammi <a.sammi1103@gmail.com>
2023-10-09 21:02:30 -0300

    aula-krexu

--- src/pages/CommentsPage.js
+++ src/pages/CommentsPage.js
@@ -3,29 +3,22 @@ import { BASE_URL } from "../constants/constants";
 import axios from "axios";
 import {Title,PostContainer } from '../style'
 import { Card } from '../components/Card/Card'
+import { useCapturarPostagens } from "../hooks/useCapturarPostagens";
+import { useRequestData } from "../hooks/useRequestData";
 
 
 const  CommentsPage = () => {
-  const [postagens, setPostagens] = useState([]);
-
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}comments`)
-      .then((response) => {
-        setPostagens(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  // const [posts] = useCapturarPostagens()
+  const [posts, error, isLoading] = useRequestData([], `${BASE_URL}comment`)
 
   return (
     <div>
       <Title>Comentários dos usuários</Title>
       <PostContainer>
-
-      {postagens.map((post) => {
+      {isLoading && <p>Carregando...</p>}
+      {!isLoading && error && <p>Ocorreu um erro. Tente novamente mais tarde.</p>}
+      {!isLoading && !error && posts && posts.length === 0 && <p>Lista vazia</p>}
+      {posts.map((post) => {
         //console.log(post);
         return(
           <Card 
--- src/pages/UserNamesPage.js
+++ src/pages/UserNamesPage.js
@@ -3,27 +3,21 @@ import { BASE_URL } from "../constants/constants";
 import axios from "axios";
 import {Title,NameContainer } from '../style'
 import { Card } from '../components/Card/Card'
+import { useCapturarNome } from "../hooks/useCapturarNomes";
+import { useRequestData } from "../hooks/useRequestData";
 
 
 const UserNamesPage = () => {
-  const [nomeUsuarios, setNomeUsuarios] = useState([]);
- 
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}users`)
-      .then((response) => {
-        setNomeUsuarios(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+//  const {nomeUsuarios} = useCapturarNome()
+const [nomeUsuarios, error, isLoading] = useRequestData([], `${BASE_URL}users`)
 
   return (
     <div>
       <Title>Nomes dos usuários</Title>
       <NameContainer>
+        {isLoading && <p>Carregando...</p>}
+        {!isLoading && error && <p>Ocorreu um erro. Tente novamente mais tarde.</p>}
+        {!isLoading && nomeUsuarios && nomeUsuarios.length === 0 && <p>Lista vazia</p>}
         {nomeUsuarios.map((usuario) => {
           return(
           <Card 
